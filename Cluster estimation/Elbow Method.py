be5d0027355b8472c53dcc5cb34ca937e15a94ec
Edwin U Kannanaikkal <edwinforedwin@gmail.com>
2020-04-05 18:33:15 +0530

    Create Elbow Method.py

---
+++ Cluster estimation/Elbow Method.py
@@ -0,0 +1,13 @@
+#Elbow Method
+
+wcss = []
+for i in range(1, 11):
+    km = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+    km.fit(X)
+    wcss.append(km.inertia_)
+    
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method', fontsize = 20)
+plt.xlabel('No. of Clusters')
+plt.ylabel('wcss')
+plt.show()
